fdea0a9196b52939d41d42015b5e846df283c450
merefield <merefield@gmail.com>
2026-09-09 16:00:25 +0100

    FIX: Recognize submission success and identify approving admins

--- app/serializers/spam_guard_submission_serializer.rb
+++ app/serializers/spam_guard_submission_serializer.rb
@@ -9,4 +9,10 @@ class SpamGuardSubmissionSerializer < ApplicationSerializer
              :completed_at,
              :error_code,
              :events
+
+  def events
+    usernames =
+      User.where(id: object.events.map { |event| event["actor_id"] }).pluck(:id, :username).to_h
+    object.events.map { |event| event.merge("actor_username" => usernames[event["actor_id"]]) }
+  end
 end
